ef99be8e6661e36b290350ab1b68d7b1a15fdd4b
thachhc <144752717+thachhc@users.noreply.github.com>
2024-11-10 02:47:13 +0700

    đã sửa lỗi và  migrate rồi

--- config/database.php
+++ config/database.php
@@ -55,8 +55,8 @@ return [
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
+            'strict' => false,
+            'engine' => env('DB_ENGINE', 'InnoDB'),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
